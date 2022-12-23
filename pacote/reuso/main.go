1baca189feb79a1970310fb45515a721dae20c7b
Roger Toledo <roger.toledo@olxbr.com>
2022-12-23 14:49:15 -0300

    pacote

---
+++ pacote/reuso/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/cod3r/area"
+	"github.com/cod3rcursos/goarea"
+)
+
+func main() {
+	fmt.Println(area.Circulo(6.0))
+	fmt.Println(area.Retangulo(5.0, 2.0))
+
+	fmt.Println(goarea.Circ(6.0))
+}
